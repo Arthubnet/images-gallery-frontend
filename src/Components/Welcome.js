1f51555ca7e9fd87b88ac1a042868aa46d7a10a3
Lena <z.otig@yahoo.com>
2022-07-20 05:26:13 -0600

    styles

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./welcome.styles.css";
 
 const Welcome = () => (
   <div className="bg-light p-5 rounded-lg m-3">
@@ -9,24 +10,17 @@ const Welcome = () => (
         Unsplash
       </a>
       &nbsp;with help of my Express REST API. In order to start, enter any
-      search term in the input field and get the random photo. After
-      saving("save" button) a picture, it's going to be stored in our cloud
-      database MongoDB Atlas(there is another version of the&nbsp;
-      <a href="https://github.com/Arthubnet/images-gallery" target="__blank">
-        app
-      </a>
-      &nbsp;on my&nbsp;
-      <a href="https://github.com/Arthubnet" target="__blank">
-        GitHub
-      </a>
-      &nbsp;, with Docker containers and local MongoDB), in order to delete it
-      simply press "Delete" button. "Remove" button removes the picture only
-      from the state and current session, if the picture was saved, page
-      reloading will return it due to useEffect hook that Fetches images from
-      our database.
+      search term in the input field and get the&nbsp;
+      <span className="random">random</span> photo. After saving("save" button)
+      a picture, it's going to be stored in our cloud database MongoDB Atlas, in
+      order to delete it simply press "Delete" button. "Remove" button removes
+      the picture only from the state and current session, if the picture was
+      saved, page reloading will return it due to useEffect hook that Fetches
+      images from our database.
       <br />
       <br />
-      Attention: all the found images depend on&nbsp;
+      <span className="attention">Attention:</span> all the found images depend
+      on&nbsp;
       <a href="http://www.unsplash.com" target="__blank">
         Unsplash
       </a>
